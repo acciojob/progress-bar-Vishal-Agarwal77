039296fbd0861193dad0a229de997d952a5c1dd2
Vishal-Agarwal77 <108511250+Vishal-Agarwal77@users.noreply.github.com>
2023-11-23 02:43:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+const prev=document.getElementsByClassName("prev");
+const next=document.getElementsByClassName("next");
+const progress=document.querySelectorAll("#circles *");
+console.log(prev);
+console.log(next);
+console.log(progress);
+if(prev.disabled===false){
+	prev
+}
